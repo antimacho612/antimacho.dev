3cee56a499a14e6a23edd5d944a901d6ddf4be7e
ttatto <zwcxk5131@gmail.com>
2024-02-08 13:28:12 +0900

    update

--- .vitepress/theme/utils/utils.ts
+++ .vitepress/theme/utils/utils.ts
@@ -39,14 +39,5 @@ export const getLangImage = (lang?: string): string | undefined => {
 };
 
 export const getDomainFromUrl = (url: string) => {
-  let result;
-  let match = url.match(/^(?:https?:\/\/)?(?:[^@\n]+@)?(?:www\.)?([^:/\n?=]+)/im);
-  if (match) {
-    result = match[1];
-    match = result.match(/^[^.]+\.(.+\..+)$/);
-    if (match) {
-      result = match[1];
-    }
-  }
-  return result;
+  return new URL(url).hostname;
 };
